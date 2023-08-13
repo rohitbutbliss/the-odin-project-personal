0050bfc44cdc92031f85768a59537b652b6f9a95
bliss <bliss@Rohits-MacBook-Air.local>
2023-08-13 13:15:57 +0530

    Started practicing javascript -learnt about let,const & var - learnt how to add external js & internal js to html

---
+++ practice/javascript-foundation-01/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+let num = 5;
+
+let string = "testing";
+
+let sub_string = string.substring(-15, 1);
+let sliced_string = string.slice(3);
+console.log(sub_string);
+console.log(sliced_string);
